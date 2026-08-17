4c7981bd979dabb1eff515e1064b60983cbb0f92
gord0nf <264331696+gord0nf@users.noreply.github.com>
2026-08-17 00:07:11 -0400

    feat(cli): edit subcommand

--- cli/subcommands/edit.py
+++ cli/subcommands/edit.py
@@ -1,5 +1,15 @@
-from log import main as logger
+import os
+
+import env
+from log import FatalCliError, main as logger
 
 
-def main(_: str, args: list[str]):
-    pass
+def main(*_):
+    editor = os.getenv("VISUAL", os.getenv("EDITOR"))
+    if not editor:
+        raise FatalCliError("couldn't get editor; please define VISUAL or EDITOR")
+
+    logger.debug(f"editor: {editor}")
+    env.CONFIG_PATH.mkdir(parents=True, exist_ok=True)
+    os.chdir(env.CONFIG_PATH)
+    os.execvp(editor, [editor, "blueprint.yaml"])
