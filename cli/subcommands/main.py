e16a7cf1b827ffae67f01c7277405f6ff5bdfd77
gord0nf <264331696+gord0nf@users.noreply.github.com>
2026-08-18 01:24:37 -0400

    wip wip main subcommand

--- cli/subcommands/main.py
+++ cli/subcommands/main.py
@@ -1,5 +1,22 @@
 from log import main as logger
 
 
-def main(_: str, args: list[str]):
-    pass
+class PackageManagement:
+    def __init__(self) -> None:
+        pass
+
+
+class PackageConfiguration:
+    def __init__(self) -> None:
+        pass
+
+
+def main(subcommand: str, scope_packages: list[str]):
+    if not subcommand:
+        subcommand = "clean install config"
+
+    clean = False
+    install = False
+    reinstall_instead_of_update = False
+    config = False
+    config_packages = False
