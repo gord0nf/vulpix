458c97e6567a675ffb078e9b5c38fc45272d906d
gord0nf <264331696+gord0nf@users.noreply.github.com>
2026-08-18 23:59:39 -0400

    SAVE WIP dotfiles subcommand (before blueprint then fix dotfiles to use blueprint)

--- cli/shell.py
+++ cli/shell.py
@@ -1,3 +1,104 @@
+import logging
+import os
+import shutil
+import subprocess
+from pathlib import Path
+
+
+def safe_copy_merge(src: Path, dst: Path):
+    def copy_without_overwrite(src, dst):
+        if not os.path.exists(dst):
+            shutil.copy2(src, dst)
+
+    shutil.copytree(src, dst, dirs_exist_ok=True, copy_function=copy_without_overwrite)
+
+
+def recursive_chmod(dir, file_mode=0o644, dir_mode=0o755):
+    for root, dirs, files in os.walk(dir):
+        for d in dirs:
+            os.chmod(os.path.join(root, d), dir_mode)
+        for f in files:
+            os.chmod(os.path.join(root, f), file_mode)
+
+
+def verify(prompt: str) -> bool:
+    return input(prompt + " (y/n): ").lower().strip().startswith("y")
+
+
+# https://jon.sprig.gs/blog/post/8025
+class RunCommand:
+    command = ""
+    cwd = ""
+    running_env = {}
+    stdout = []
+    stderr = []
+    exit_code = 999
+
+    def __init__(
+        self,
+        command: list = [],
+        cwd: str | None = None,
+        env: dict | None = None,
+        raise_on_error: bool = True,
+        logger: logging.Logger | None = None,
+    ):
+        self.command = command
+        self.cwd = cwd
+
+        self.running_env = os.environ.copy()
+
+        if env is not None and len(env) > 0:
+            for env_item in env.keys():
+                self.running_env[env_item] = env[env_item]
+
+        if logger:
+            logger.debug(f'exec: {" ".join(command)}')
+
+        try:
+            result = subprocess.run(
+                command,
+                cwd=cwd,
+                capture_output=True,
+                text=True,
+                check=True,
+                env=self.running_env,
+            )
+            # Store the result because it worked just fine!
+            self.exit_code = 0
+            self.stdout = result.stdout.splitlines()
+            self.stderr = result.stderr.splitlines()
+        except subprocess.CalledProcessError as e:
+            # Or store the result from the exception(!)
+            self.exit_code = e.returncode
+            self.stdout = e.stdout.splitlines()
+            self.stderr = e.stderr.splitlines()
+
+        # If verbose mode is on, output the results and errors from the command execution
+        if len(self.stdout) > 0 and logger:
+            logger.debug(f"stdout: {"\n".join(self.stdout)}")
+        if len(self.stderr) > 0 and logger:
+            logger.debug(f"stderr: {"\n".join(self.stderr)}")
+
+        # If it failed and we want to raise an exception on failure, record the command and args
+        # then Raise Away!
+        if raise_on_error and self.exit_code > 0:
+            raise Exception(
+                f"Error ({self.exit_code}) running command: {command}\nstderr: {self.stderr}\nstdout: {self.stdout}"
+            )
+
+    def __repr__(self) -> str:  # Return a string representation of this class
+        return "\n".join(
+            [
+                f"Command: {self.command}",
+                f"Directory: {self.cwd if not None else '{current directory}'}",
+                f"Env: {self.running_env}",
+                f"Exit Code: {self.exit_code}",
+                f"nstdout: {self.stdout}",
+                f"stderr: {self.stderr}",
+            ]
+        )
+
+
 # https://gist.github.com/rene-d/9e584a7dd2935d0f461904b9f2950007
 class Colors:
     BLACK = "\033[0;30m"
--- cli/subcommands/dotfiles.py
+++ cli/subcommands/dotfiles.py
@@ -1,5 +1,104 @@
-from log import main as logger
+import os
+from pathlib import Path
+import shutil
+
+import env, shell
+from log import FatalCliError, main as logger
+
+
+def install_fonts(fontdir: Path):
+    match env.OS:
+        case "windows":
+            pass  # TODO
+        case "linux":
+            env.FONT_INSTALL.mkdir(parents=True, exist_ok=True)
+            shell.safe_copy_merge(fontdir, env.FONT_INSTALL)
+            shell.recursive_chmod(env.FONT_INSTALL, file_mode=0o644, dir_mode=0o755)
+            if shutil.which("fc-cache") is not None:
+                shell.RunCommand(["fc-cache"], logger=logger)
+
+
+def setup_dotfiles(dotfiles: Path):
+    ignore_file = dotfiles / ".vulpixignore"
+    font_dir = dotfiles / "fonts"
+
+    # get ignored paths
+    ignored_paths = [ignore_file, font_dir]
+    if ignore_file.exists():
+        with open(ignore_file, "r") as f:
+            for line in f:
+                ignored_paths.append(dotfiles.joinpath(line))
+    ignored_paths = [i for i in ignored_paths if i.exists()]
+
+    logger.debug(f"ignoring: {ignored_paths}")
+
+    # compile link map
+    links: dict[Path, Path] = {}  # like [target_path]=link_path
+    for item in dotfiles.iterdir():
+        if any(item.samefile(i) for i in ignored_paths):
+            continue
+        if item.is_dir() and item.name == ".config":
+            for tool in item.iterdir():
+                if any(tool.samefile(i) for i in ignored_paths):
+                    continue
+                links[tool] = Path.home() / ".config" / tool.name
+            continue
+
+        links[item] = Path.home() / item.name
+
+    logger.debug(f"links: {links}")
+
+    # make sure we're not overwritting anything
+    existing_items: list[Path] = []
+    for target, link in links.items():
+        if link.exists() and link.samefile(target):
+            logger.debug(f"removing existing link: {link}")
+            link.unlink()
+        if link.exists():
+            existing_items.append(link)
+    if len(existing_items) > 0:
+        print(
+            "The following items will be overwritting and replaced by links:\n"
+            + "\n".join(["  - " + str(i) for i in existing_items])
+            + "\n"
+        )
+        if not shell.verify("Are you sure you want to continue?") or not shell.verify(
+            "All these items will be deleted..."
+        ):
+            raise FatalCliError("aborted")
+        for item in existing_items:
+            os.remove(item)
+        print()  # style
+
+    raise SystemExit(1)
+
+    # apply symlinks
+    if len(links) > 0:
+
+        for target, link in links.items():
+            # _print_pretty_link "$item"
+            link.symlink_to(target)
+    else:
+        logger.info("nothing to symlink")
+
+    # install fonts
+    if font_dir.exists():
+        print()  # style
+        logger.info(f"installing fonts from '{font_dir}'")
+        install_fonts(font_dir)
 
 
 def main(_: str, args: list[str]):
-    pass
+    dotfiles_path: Path | None = None
+    match len(args):
+        case 0:
+            pass
+        case 1:
+            dotfiles_path = Path(args[0])
+        case _:
+            raise FatalCliError("invalid number of args")
+    if not dotfiles_path:
+        raise FatalCliError("please specify dotfiles dir in args or blueprint")
+
+    logger.debug(f"dotfiles: {dotfiles_path}")
+    setup_dotfiles(Path(dotfiles_path))
